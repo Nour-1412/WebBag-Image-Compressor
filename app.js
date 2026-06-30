a9616475a216c8fe6df95e2754964eabdeb92534
Nour-1412 <nourelain796@gmail.com>
2026-06-30 13:32:56 +0300

    Fix alert order for image input and select image

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-alert("app.js loaded");
 const imageInput = document.getElementById("imageInput");
 const selectImage = document.getElementById("selectImage");
 
-alert(selectImage);
 alert(imageInput);
+alert(selectImage);
