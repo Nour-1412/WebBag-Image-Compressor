c5f2bc73238ff9f5175190f9a7a5c539d988753e
Nour-1412 <nourelain796@gmail.com>
2026-06-30 13:02:56 +0300

    Implement file picker opening on button click
    Added event listener to open file picker on click.

---
+++ app.js
@@ -0,0 +1,22 @@
+// ==========================
+// ELEMENTS
+// ==========================
+
+const imageInput = document.getElementById("imageInput");
+
+const selectImage = document.getElementById("selectImage");
+
+const dropArea = document.getElementById("dropArea");
+
+const originalPreview = document.getElementById("originalPreview");
+
+const originalSize = document.getElementById("originalSize");
+// ==========================
+// OPEN FILE PICKER
+// ==========================
+
+selectImage.addEventListener("click", () => {
+
+    imageInput.click();
+
+});
